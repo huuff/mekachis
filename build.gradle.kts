8c49673ad424f94219d1bac7d5d49da5ff5f9e15
Haf <haf@protonmail.ch>
2022-08-12 12:59:26 +0200

    publishing to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm") version "1.6.21" apply false
     id("org.jetbrains.kotlinx.kover") version "0.5.0" apply false
+    id("maven-publish")
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("signing")
 }
 
 allprojects {
@@ -13,12 +16,27 @@ allprojects {
 }
 
 subprojects {
-
     afterEvaluate {
         tasks.withType<Test> {
             useJUnitPlatform()
             jvmArgs("--add-opens", "java.base/java.time=ALL-UNNAMED")
             finalizedBy(tasks.getByName("koverHtmlReport"))
+        }
+
+        project.extensions.findByType(JavaPluginExtension::class.java)?.apply {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(properties["sonatype.user"] as String)
+            password.set(properties["sonatype.password"] as String)
         }
     }
 }
--- redis/build.gradle.kts
+++ redis/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm")
     id("org.jetbrains.kotlinx.kover")
+    id("maven-publish")
+    id("signing")
 }
 
 val kotestVersion = "5.4.1"
@@ -18,4 +20,45 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation("io.mockk:mockk:1.12.5")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                packaging = "jar"
+                name.set(project.name)
+                description.set("Redis-backed cache implementation with Me-Kachis! interfaces")
+
+                url.set("https://github.com/huuff/mekachis")
+                scm {
+                    connection.set("scm:git:git://github.com/huuff/mekachis.git")
+                    developerConnection.set("scm:git:git@github.com:huuff/mekachis.git")
+                    url.set("https://github.com/huuff/mekachis/tree/master")
+                }
+
+                licenses {
+                    license {
+                        name.set("WTFPL - Do What The Fuck You Want To Public License")
+                        url.set("http://www.wtfpl.net")
+                    }
+                }
+
+                developers {
+                    developer {
+                        name.set("Francisco Sánchez")
+                        email.set("haf@protonmail.ch")
+                        organizationUrl.set("https://github.com/huuff")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
